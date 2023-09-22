d553c5c04ba1240517d634e6b8878490ece56aef
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-09-22 09:58:38 +0300

    Phase-0-Objects-Lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee= {
+    name:`Levin`,
+    streetAddress: `Juja`
+}
+
+function updateEmployeeWithKeyAndValue(obj,key,value) {
+    const newEmployee={...employee};
+    newEmployee[key]=value
+    return newEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key]=value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(obj,key) {
+    let newEmployee={...employee};
+    delete newEmployee.name;
+    return newEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(obj,key) {
+    delete employee.name;
+    return employee;
+};
