da1a10f8bab22488fae3e71f54e3de5e36db9878
Levin-ops <artmwanganyi09@outlook.com>
2023-09-29 01:57:05 +0300

    Phase 1 Objects Lab

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 // Write your solution in this file!
-const employee= {
-    name:`Levin`,
-    streetAddress: `Juja`
-}
-
-function updateEmployeeWithKeyAndValue(obj,key,value) {
-    const newEmployee={...employee};
-    newEmployee[key]=value
-    return newEmployee;
+const employee = {
+  name: `Levin`,
+  streetAddress: `Juja`,
 };
-
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+  const newEmployee = { ...employee };
+  newEmployee[key] = value;
+  return newEmployee;
+}
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key]=value;
+  employee[key] = value;
 
-    return employee;
+  return employee;
 }
-
-function deleteFromEmployeeByKey(obj,key) {
-    let newEmployee={...employee};
-    delete newEmployee.name;
-    return newEmployee;
-};
-
-function destructivelyDeleteFromEmployeeByKey(obj,key) {
-    delete employee.name;
-    return employee;
-};
+function deleteFromEmployeeByKey(obj, key) {
+  let newEmployee = { ...employee };
+  delete newEmployee.name;
+  return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+  delete employee.name;
+  return employee;
+}
